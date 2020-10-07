c62c1907d21d8239e58048e35c1495bb5cfb1a5d
shiweing <shiweing1998@gmail.com>
2020-10-07 22:37:42 +0800

    Basic navbar

---
+++ view/src/components/Compare.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Compare extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default Compare;
---
+++ view/src/components/Generate.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Generate extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Generate</h1>
+            </div>
+        )
+    }
+}
+
+export default Generate;
