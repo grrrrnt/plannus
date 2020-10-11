1ad88f8a1f35d58860e8b69d23354859f34a802b
etlow <42794246+etlow@users.noreply.github.com>
2020-10-11 23:37:20 +0800

    Get module list

--- view/src/components/Generate.jsx
+++ view/src/components/Generate.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 
+import SelectModules from "./SelectModules";
+
 class Generate extends Component {
     render() {
         return (
             <div>
                 <h1>Generate</h1>
+                <SelectModules />
             </div>
         )
     }
